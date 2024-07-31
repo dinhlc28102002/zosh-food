0a4a874ac885cf7e7d470cefc1a48eb497cb398e
Le Cong Dinh <hobao3210@gmail.com>
2024-07-31 22:56:56 +0700

    update config model and security

---
+++ src/main/java/com/zosh/dto/RestaurantDto.java
@@ -0,0 +1,20 @@
+package com.zosh.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Embeddable
+public class RestaurantDto {
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private List<String> images;
+
+    private String description;
+}
---
+++ src/main/java/com/zosh/model/Address.java
@@ -0,0 +1,12 @@
+package com.zosh.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+
+}
